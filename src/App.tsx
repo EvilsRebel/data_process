80d47fc7120612f52d9bcc508bd5c340561f0515
邱伟 <406810175@qq.com>
2024-11-26 21:35:55 +0800

    feat(tmp): 按规格拆分

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,31 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
+import { readFile } from "@tauri-apps/plugin-fs";
+import { read, utils } from "xlsx";
+import { Button } from "antd";
+
+import { open } from "@tauri-apps/plugin-dialog";
 import "./App.css";
+import { ProductFixService } from "./service/productFixService";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const handleTest = async () => {
+    const file = await open({
+      multiple: false,
+      directory: false,
+      filters: [{ extensions: ["xlsx"], name: "" }],
+    });
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    if (!file) {
+      return;
+    }
 
+    ProductFixService.processExcel(file);
+  };
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+      <Button onClick={handleTest}>测试</Button>
     </main>
   );
 }
